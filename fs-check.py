84cd9b788e84434208bbef5afd65de96cb815df9
Zane D. R <zane.reick@unknownts.com>
2022-03-24 12:15:42 -0500

    Code documentation

--- fs-check.py
+++ fs-check.py
@@ -2,7 +2,10 @@ import os, sys
 
 
 def convert_bytes(num):
-    step_unit = 1024.0
+    """
+    Convert a non-denominated value of bytes to a denominated value of bytes
+    """
+    step_unit = 1024.0 # Value to reach before incrementing denominators
     for x in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
         if num < step_unit:
             return "%3.1f %s" % (num, x)
@@ -11,13 +14,10 @@ def convert_bytes(num):
 
 Folderpath = sys.argv[1] # Take directory in
 
-size = 0
+size = 0 # Create a var to hold the file size
 
 for root, dirs, files in os.walk(Folderpath):
     for file_obj in files:
-        size += os.stat(root + "/" + file_obj).st_size
-
-#for ele in os.scandir(Folderpath):
-#    size += os.stat(ele).st_size
+        size += os.stat(root + "/" + file_obj).st_size # Add each file's size together
 
 print(convert_bytes(size))
