5297b30ff5c1b1577cfc9b688a7832df72071f65
Zane D. R <zane.reick@unknownts.com>
2022-03-25 00:51:53 -0500

    Added a post request method handler

--- webserver.py
+++ webserver.py
@@ -1,8 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import urllib
 
 class webAPI(BaseHTTPRequestHandler):
 
+#    def _set_headers(self):
+#        self.send_response(200)
+#        self.send_header('Content-type', 'text/html')
+#        self.end_headers()
+#    def do_HEAD(self):
+#        self._set_headers()
     def do_GET(self):
         if self.path == '/':
             self.path = '/index.html'
@@ -15,6 +22,15 @@ class webAPI(BaseHTTPRequestHandler):
             self.send_response(404)
         self.end_headers()
         self.wfile.write(bytes(response, 'utf-8'))
+    def do_POST(self):
+        length = int(self.headers['Content-Length'])
+        post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
+        print(post_data)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(bytes("PLACEHOLDER", 'utf-8'))
+
+
 
 def serveAPI(bind_addr, bind_port):
     ws = HTTPServer((bind_addr, bind_port), webAPI)
