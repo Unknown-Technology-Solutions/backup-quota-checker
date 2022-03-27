9fbf648d3d9d6e352d528547111cb13b3b7e0ec0
Zane D. R <zane.reick@unknownts.com>
2022-03-26 23:52:14 -0500

    Cont'd dev on handling database ops

--- database-gather.py
+++ database-gather.py
@@ -3,5 +3,41 @@ import mysql.connector
 info_db = mysql.connector.connect(
     host="localhost",
     user="infoMan",
-    password="placeholder"
+    password="placeholder",
+    database="backup_resources"
 )
+
+
+def validate_user_info(connection, username, auth_key):
+    """
+    Check if a given username and key combo is valid
+    """
+    cur = connection.cursor()
+    query = "SELECT `id` FROM auth_keys WHERE user = '" + \
+        username + "' AND auth_key = '" + auth_key + "'"
+    cur.execute(query)
+    return_array = cur.fetchone()
+    if len(return_array) == 1:
+        # return 1st value, that should be the only value
+        return return_array[0]
+    else:
+        raise ValueError
+
+
+def find_backup_dir(connection, username):
+    """
+    Find a given user's backup directory
+    """
+    cur = connection.cursor()
+    query = "SELECT `directory` FROM user_mapping WHERE username = '" + username + "'"
+    cur.execute(query)
+    return_array = cur.fetchone()
+    if len(return_array) == 1:
+        # return 1st value, that should be the only value
+        return return_array[0]
+    else:
+        raise ValueError
+
+
+print(validate_user_info(info_db, "zane.reick",
+      "23d72d65-ad82-11ec-a614-020054746872"))
