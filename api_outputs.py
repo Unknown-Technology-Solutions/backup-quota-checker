9cb27a8c5d470489f2193c603e526870cde0804b
Zane D. R <zane.reick@unknownts.com>
2022-03-28 00:21:59 -0500

    Added the ability to read usage data

--- api_outputs.py
+++ api_outputs.py
@@ -9,10 +9,31 @@ def auth_from_json(db_con, json_in):
     """
     Expected JSON input:
     { "username":"foo", "key":"bar"}
+    This will return an int if user is valid, False if invalid
     """
     in_dict = json.loads(json_in)
     return fsdb.dg.validate_user_info(db_con, in_dict["username"], in_dict["key"])
 
 
-#print(auth_from_json(fsdb.dg.auth_to_db("infoMan", "placeholder"),
-#      '{"username": "zane.reick", "key": "23d72d65-ad82-11ec-a614-020054746872"}'))
+def read_usage_data(db_con, json_in):
+    """
+    Expected JSON input:
+    { "username":"foo", "key":"bar"}
+    """
+    is_user_valid = auth_from_json(db_con, json_in)
+    in_dict = json.loads(json_in)
+
+    if type(is_user_valid) is int:
+        bytes_used = fsdb.rw_usage(db_con, in_dict["username"])
+        bytes_quota = fsdb.dg.find_data_cap(db_con, in_dict["username"])
+
+        output = '{ "usage": "' + \
+            str(bytes_used) + '", "quota": "' + str(bytes_quota) + '"}'
+        return output
+    elif is_user_valid == False:
+        output = '{ "usage": "-1", "quota": "-1"}'
+        return output
+
+
+print(read_usage_data(fsdb.dg.auth_to_db("infoMan", "placeholder"),
+      '{"username": "zane.reick", "key": "23d72d65-ad82-11ec-a614-020054746872"}'))
