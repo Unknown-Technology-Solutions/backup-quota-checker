21e1445a61117fd4fdf635dd58de0e26aaf96aff
Zane D. R <zane.reick@unknownts.com>
2022-04-05 09:30:20 -0500

    Add an easy way to run the service

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
 from webserver import serveAPI
+from sys import argv
 
-# Testing
-
-bind_addr, bind_port = '127.0.0.1', 8080
+try:
+    if type(argv[1]) is str and type(int(argv[2])) is int:
+        bind_addr, bind_port = argv[1], int(argv[2])
+    else:
+        bind_addr, bind_port = '0.0.0.0', 8080
+except:
+    bind_addr, bind_port = '0.0.0.0', 8080
 
 serveAPI(bind_addr, bind_port)
